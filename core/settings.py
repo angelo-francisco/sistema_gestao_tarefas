903de43db7d70a3019a12787f7338cb6d4a55b46
angelo-francisco <ics20080729@gmail.com>
2024-12-24 14:03:24 +0100

    static added

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -131,6 +131,7 @@ STORAGES = {
 
 WHITE_NOISE_MAX_AGE = 31536000  # 1 ano
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.base import RedirectView
@@ -9,7 +7,7 @@ urlpatterns = [
     path("auth/", include("accounts.urls"), name="accounts"),
     path("task/", include("tasks.urls"), name="tasks"),
     path("", RedirectView.as_view(url=reverse_lazy("task_list_view"))),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 
 handler404 = "tasks.views.handler404"
